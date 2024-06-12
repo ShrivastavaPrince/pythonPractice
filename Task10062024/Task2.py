53e1264f103ea23ba2dc67a7ccb460ee3715ffd7
Prince <shrivastavaprince01@gmail.com>
2024-06-12 15:14:22 +0530

    Task

---
+++ Task10062024/Task2.py
@@ -0,0 +1,21 @@
+#Task #2
+
+#Script 1: Calculate Square and Cube
+
+num = 2
+square = num ** 2
+cube = num ** 3
+print("Square of", num, "is:", square)
+print("Cube of", num, "is:", cube)
+
+#Script 2: Compare Two Numbers
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num1 > num2:
+    print(num1, "is greater than", num2)
+elif num1 < num2:
+    print(num1, "is less than", num2)
+else:
+    print(num1, "is equal to", num2)
