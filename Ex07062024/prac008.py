69d37526b2583890ab42df0ed00bd693d5df3a47
Prince <shrivastavaprince01@gmail.com>
2024-06-12 14:13:56 +0530

    string properties

---
+++ Ex07062024/prac008.py
@@ -0,0 +1,13 @@
+#immutability
+x = "Prince"
+x = x + " How are you"
+print(x)
+
+x = "Hello World"
+print(x.upper())
+print(x.lower())
+print(x.split())
+
+x= "Hi this is a string"
+print(x.split())
+print(x.split('i'))
