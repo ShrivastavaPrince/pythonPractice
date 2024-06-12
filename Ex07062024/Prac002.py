ca168d11b39dd49e525ed75e9f50062ee8d8772e
Prince <shrivastavaprince01@gmail.com>
2024-06-12 13:11:29 +0530

    Practice

---
+++ Ex07062024/Prac002.py
@@ -0,0 +1,11 @@
+# Take 2 int num from user and add
+
+# Taking two integer inputs from the user
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+# Adding the two integers
+sum = num1 + num2
+
+# Printing the result
+print("The sum of {} and {} is {}".format(num1, num2, sum))
---
+++ Ex07062024/Prac003.py
@@ -0,0 +1,9 @@
+# Taking two string inputs from the user
+str1 = input("Enter the 1st string: ")
+str2 = input("Enter the 2nd string: ")
+
+# Concatenating the two strings
+result = str1 + str2
+
+# Printing the result
+print(result)
