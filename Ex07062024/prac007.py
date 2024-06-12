75c2de78ee8cf2f95e8b729d539c2dfab88c9c06
Prince <shrivastavaprince01@gmail.com>
2024-06-12 13:48:50 +0530

    slicing & step size

--- Ex07062024/prac007.py
+++ Ex07062024/prac007.py
@@ -4,3 +4,11 @@ print(mystring[2:])
 print(mystring[:3])
 print(mystring[3:6])
 print(mystring[1:5])
+
+#jump stp size
+
+print(mystring[::4])
+
+#print(mystring[start: stop : step size])
+
+print(mystring[2:8:3])
